ddbab598b199f6a3173223a4b39b1604548acf21
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-07-31 20:09:46 +0200

    add changelog

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -62,6 +62,7 @@ const HomePage = () => {
           </button>
         </Form>
       </details>
+      <a href="/changelog">See changelog</a>
     </div>
   );
 };
---
+++ app/routes/changelog.tsx
@@ -0,0 +1,18 @@
+const ChangelogPage = () => {
+  return (
+    <>
+      <h1>Changelog</h1>
+      <ul>
+        <li>
+          <b>0.0.1</b>, 31.07.2023 - added dates
+        </li>
+        <li>
+          <b>0.0.0</b>, 30.07.2023 - initial version of the service - added
+          boards, threads and replies with text only
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default ChangelogPage;
