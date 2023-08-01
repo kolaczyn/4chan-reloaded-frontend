825428c55caf54397c654c243f343b01d30fcd4e
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-08-01 18:13:37 +0200

    update changelog

--- app/routes/changelog.tsx
+++ app/routes/changelog.tsx
@@ -1,15 +1,38 @@
+type ChangelogItem = {
+  version: string;
+  date: string;
+  changes: string;
+};
+
+const changelog: ChangelogItem[] = [
+  {
+    version: "0.0.3",
+    date: "01.08.2023",
+    changes: "added pagination for threads of a board",
+  },
+  {
+    version: "0.0.1",
+    date: "31.07.2023",
+    changes: "added dates",
+  },
+  {
+    version: "0.0.0",
+    date: "30.07.2023",
+    changes:
+      "initial version of the service - added boards, threads and replies with text only",
+  },
+];
+
 const ChangelogPage = () => {
   return (
     <>
       <h1>Changelog</h1>
       <ul>
-        <li>
-          <b>0.0.1</b>, 31.07.2023 - added dates
-        </li>
-        <li>
-          <b>0.0.0</b>, 30.07.2023 - initial version of the service - added
-          boards, threads and replies with text only
-        </li>
+        {changelog.map((x) => (
+          <li key={x.version}>
+            <b>{x.version}</b>, {x.date} - {x.changes}
+          </li>
+        ))}
       </ul>
     </>
   );
